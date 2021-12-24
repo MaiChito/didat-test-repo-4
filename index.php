d7973d8c21a944b620052ea66f48087fd078aa0e
Ahmad Didat <didat11.6g@gmail.com>
2021-12-24 14:41:53 +0700

    menambah list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates sequi blanditiis dignissimos et! Nobis voluptatum asperiores itaque at magnam velit amet impedit, ullam animi provident!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
